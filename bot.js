5fbdf0c31c9b5ea28dfb915fffe7dc8267554dd1
M4sterek <damiannq1337@gmail.com>
2021-02-20 00:16:37 +0100

    update

--- bot.js
+++ bot.js
@@ -17,6 +17,7 @@ client.on('ready', () => {
   console.log(chalk.red('<============================================>'))
   console.log(chalk.red('Logged in as',client.user.tag))
   console.log(chalk.red('<============================================>'))
+  client.user.setActivity(">>help", {type: 'STREAMING'})
 });
 
 
