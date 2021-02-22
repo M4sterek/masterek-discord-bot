d19653a9f7d27b919e879ef7f0ad0787d0fbc658
M4sterek <damiannq1337@gmail.com>
2021-02-22 09:01:04 +0100

    update

--- commands/(V.I.P)help.command.js
+++ commands/(V.I.P)help.command.js
@@ -5,6 +5,7 @@ const {prefix} = require(__dirname+"/../config/config.js")
 module.exports = {
     name: "help",
     description: "Show commands!",
+    args: true,
     usage: "[command]",
     example: "clear",
     run(msg,args){
@@ -13,13 +14,24 @@ module.exports = {
         if(!args.lenght){
             data.push("There's list of commands")
             data.push(commands.map((cmd) => cmd.name).join(", "))
+            return msg.author.send(data)
+            .then(()=>{
+                if(msg.channel.type==="dm") return
+                 msg.channel.send("📄 | **I've sent a list of command on dm!**")
+            })
+            .catch(err =>{
+                console.error(`Couldn't send dm to ${msg.author.tag}\n`, err)
+                msg.reply("❌ | It seems I can't dm you! Do you have DMs disabled?")
+            })
+        
         }
-        msg.author.send(data)
-        .then(()=>{
-            if(msg.channel.type==="dm") return
-            msg.channel.send("📄 | **I've sent a list of command on dm!**")
-
-        })
+        
+        const cmdName = args[0].toLowerCase()
+        const cmd = commands.get(cmdName) || commands.find(c => c.aliases && c.aliases.include(cmdName))
+        if(!cmd){
+            msg.channel.send("❌ | Command doesn't exist")
+        
+        }
         
     }
 }
